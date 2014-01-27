1f2276bc0318f0e6d015b99ed65e86e71ff9ec54
Peter Bengtsson <mail@peterbe.com>
2014-01-27 09:01:17 -0800

    hacking on grunt but getting littlewhere

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,27 +4,28 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    concat: {
-       options: {
-          separator: ';',
-       },
-      dist: {
-         src: ['client/static/js/*.js',
-               //'client/static/js/md5.js',
-               //'client/static/js/angularforage.js',
-               //'client/static/js/utils.js',
-               //'client/static/js/buggy.js',
-               ],
-          dest: 'dist/build-<%= grunt.template.today("yyyy-mm-dd") %>.js',
-      },
-    },
+//    concat: {
+//       options: {
+//          separator: ';',
+//       },
+//      dist: {
+//         src: ['client/static/js/*.js',
+//               //'client/static/js/md5.js',
+//               //'client/static/js/angularforage.js',
+//               //'client/static/js/utils.js',
+//               //'client/static/js/buggy.js',
+//               ],
+//          dest: 'dist/build-<%= grunt.template.today("yyyy-mm-dd") %>.js',
+//      },
+//    },
     uglify: {
        my_stuff: {
           options: {
              banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
           },
           files: {
-            'dist/build-<%= grunt.template.today("yyyy-mm-dd") %>.min.js':
+            //'dist/build-<%= grunt.template.today("yyyy-mm-dd") %>.min.js':
+            'dist/bundle.min.js':
               'client/static/js/*.js'
           }
        },
@@ -35,7 +36,8 @@ module.exports = function(grunt) {
              preserveComments: true
           },
           files: {
-            'dist/vendor-<%= grunt.template.today("yyyy-mm-dd") %>.min.js':
+            //'dist/vendor-<%= grunt.template.today("yyyy-mm-dd") %>.min.js':
+            'dist/vendor.min.js':
               'client/static/js/vendor/*.js'
           }
        }
@@ -43,7 +45,8 @@ module.exports = function(grunt) {
     cssmin: {
       compress: {
         files: {
-          'dist/all-<%= grunt.template.today("yyyy-mm-dd") %>.min.css':
+          //'dist/all-<%= grunt.template.today("yyyy-mm-dd") %>.min.css':
+          'dist/combined.min.css':
             ['client/static/css/pure-min.css',
              'client/static/css/email.css',
              'client/static/css/extra.css']
@@ -61,26 +64,46 @@ module.exports = function(grunt) {
 //        jshintrc: '.jshintrc'
 //      }
     },
+    copy: {
+       html: {
+          files: [
+                  {
+                    'dist/index.html': ['client/index.html']
+                  }
+                 ]
+       },
+      sounds: {
+         files: [
+                 {expand: true, flatten: true, src: ['client/static/sounds/*'], dest: 'dist/'},
+                ]
+      }
+    },
     useminPrepare: {
-       html: 'client/index.html'
+       html: 'client/index.html',
+        options: {
+           root: 'client',
+           dest: 'dist'
+        }
     },
     usemin: {
-       html: 'client/index.html'
-    }
+       html: 'dist/index.html',
+        options: {
+           //dest: 'dist-foo',
+           //dirs: ['dist']
+        }
+    },
   });
 
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-assets-versioning');
   grunt.loadNpmTasks('grunt-usemin');
-//  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-//  grunt.loadNpmTasks('grunt-contrib-requirejs');
-//  grunt.loadNpmTasks('grunt-contrib-watch');
-//  grunt.registerTask('build', ['cssmin', 'requirejs', 'concat']);
-  //grunt.registerTask('build', ['cssmin', 'uglify', 'concat']);
-  grunt.registerTask('build', ['usemin']);
-//  grunt.registerTask('default', ['jshint', 'build', 'nodeunit']);
+  grunt.loadNpmTasks('grunt-copy');
+
+  grunt.registerTask('build', ['copy', 'useminPrepare', 'uglify', 'cssmin', 'concat', 'usemin']);
+
   grunt.registerTask('default', ['jshint', 'build']);
 //  grunt.registerTask('travis', ['jshint', 'nodeunit']);
 };
